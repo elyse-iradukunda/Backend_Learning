ce452527697765b93bfe18820881e23ad6ab6454
Your Name <ellycreativity8@gmail.com>
2026-04-20 11:20:07 +0200

    here is all about solving kata

--- node/home.js
+++ node/home.js
@@ -1,9 +1,18 @@
-const  people =["iran","burundi","rwanda","kenya"];
-console.log(people)
+// const  people =["iran","burundi","rwanda","kenya"];
+// console.log(people)
 
 
-  export function Phone(){
-    console.log("here is phone")
+//   export function Phone(){
+//     console.log("here is phone")
+//   }
+
+//   Phone()
+
+
+ function lengths(str){
+   const n= str.split(" ");
+   console.log(n)
+   return  [...n].length
   }
 
-  Phone()
+ console.log(lengths("I love you very well"));
